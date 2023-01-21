0237b449ef1d82937c0b697f6ef0454746735878
Shreyas S <104297402+ShreyasS07@users.noreply.github.com>
2023-01-21 15:27:50 +0530

    Update Terrain_GST.py

--- Terrain_GST.py
+++ Terrain_GST.py
@@ -16,25 +16,6 @@ def select_folder():
     for file in file_list:
         print(file)
 
-    # # Creating separate dataframes for csv and xlsx files
-    # csv_df = pd.DataFrame()
-    # xlsx_df = pd.DataFrame()
-    # files = os.listdir(folder_path)
-    # for file in files:
-    #     if file.endswith('.csv'):
-    #         df = pd.read_csv(os.path.join(folder_path, file))
-    #         print("Dataframe shape for", file, ":", df.shape)
-    #     elif file.endswith('.xlsx'):
-    #         df = pd.read_excel(os.path.join(folder_path, file))
-    #         print("Dataframe shape for", file, "is :", df.shape)
-    # # Combining all dataframes into single dataframe
-    # final_df = pd.concat([csv_df, xlsx_df])
-    # print("Shape of final dataframe:", final_df.shape)
-
-    # df1 = pd.read_excel(r"C:\Users\ASUS\Downloads\Nov GST Set\Nov GST Set\Sales 08th to 10th Nov-22.xlsb.xlsx")
-    # df1.head()
-    # df1.isnull().sum()
-
     # Create dataframes for each CSV and XLSX file
     for file in file_list:
         if file.endswith(".csv"):
@@ -63,57 +44,6 @@ def select_folder():
     final_df.to_excel(os.path.join(folder_path, "Final report.xlsx"), index=False)
     print("All Excel files extracted & saved to single excel file & stored in same folder")
     print("Final report saved at:", os.path.join(folder_path, "Final report.xlsx"))
-
-    # #Saving final_df to xlsb format.
-    # final_df.to_excel(os.path.join(folder_path, "Final report.xlsb"), index=False)
-    #
-    # DF_Output = pd.read_excel(os.path.join(folder_path, "Final report.xlsx"))
-    # DF_Output.shape()
-
-
-
-
-
-
-    # ------------------------------------------   xlsx to xlsb  ----------------------------------------------
-
-    # Reducing the final report size by Converting into Binary Excel format.
-
-    # workbook_xlsx = openpyxl.load_workbook(folder_path, "Final report.xlsx")
-    # print("final report loaded ")
-    #
-    # # Create a new xlsb file
-    # workbook_xlsb = openpyxl.Workbook(write_only=True)
-    #
-    # # Iterate over the worksheets in the xlsx file
-    # for sheet in workbook_xlsx:
-    #     # Create a new worksheet in the xlsb file
-    #     ws_xlsb = workbook_xlsb.create_sheet(sheet.title)
-    #     # Copy the data from the xlsx worksheet to the xlsb worksheet
-    #     for row in sheet.iter_rows():
-    #         ws_xlsb.append([cell.value for cell in row])
-    #
-    # # Save the xlsb file
-    # workbook_xlsb.save(os.path.join(folder_path, "Final Binary report.xlsb"))
-    # print("Final Binary report created ")
-
-
-
-    # ------------------------------------------   Creating Pivot table  ----------------------------------------------
-
-    # # Creating Pivot table
-    # print("Creating a Pivot table for Final report")
-    #
-    # # taking created Excel file from previous code
-    # file_path = 'data/base_file.xlsx'
-    # df = pd.read_excel(file_path)
-    #
-    # # Create a pivot table
-    # pivot_table = df.pivot_table(values='Value', index='Category', columns='Year', aggfunc='sum')
-    #
-    # # Save the pivot table to a new Excel file
-    # pivot_file_path = 'data/pivot_file.xlsx'
-    # pivot_table.to_excel(pivot_file_path)
 
 
 window = Tk()
